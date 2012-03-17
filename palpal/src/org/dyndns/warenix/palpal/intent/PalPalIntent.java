b33e46e2621ae0fb245766b1939dfbcdf494d951
Warren Wong <warren.wong@enterproid.com>
2012-03-18 02:44:14 +0800

    Add missing file and update wrong comments

--- palpal/src/org/dyndns/warenix/palpal/intent/PalPalIntent.java
+++ palpal/src/org/dyndns/warenix/palpal/intent/PalPalIntent.java
@@ -7,7 +7,7 @@ public class PalPalIntent {
 	public static final String ACTION_TWITTER_QUOTE_TWEET = "org.dyndns.warenix.palpal.twitter.ACTION_QUOTE_TWEET";
 
 	/**
-	 * Action to quote tweet. Must bundle a "message" twitter4j.Status instance
+	 * Action to quote tweet. Must bundle a "message" FacebookObject instance
 	 */
 	public static final String ACTION_FACEBOOK_RESHARE_POST = "org.dyndns.warenix.palpal.facebook.ACTION_RESHARE_POST";
 
